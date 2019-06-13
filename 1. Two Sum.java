7d8263d2e2f7f9e18da1459e9e88c6584842c90e
LitteKe <46374416+LitteKe@users.noreply.github.com>
2019-06-13 16:22:03 +0800

    Update 1. Two Sum.java

--- 1. Two Sum.java
+++ 1. Two Sum.java
@@ -18,3 +18,19 @@ class Solution {
         return re;
     }
 }
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++)
+        {
+           for(int j = i + 1; j < nums.length; j++)
+           {
+               if(nums[i] + nums[j] == target)
+               {
+                   return new int[] {i,j};
+               }
+           }
+        }
+        return null;
+    }
+}
